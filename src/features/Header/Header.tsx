c7056b4bf4aabf4d0205fc2395827c8e0c3dd5b7
Yubin <yubinjodev@gmail.com>
2023-05-27 14:00:28 +0900

    scss template

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,7 +1,19 @@
+
 export default function Header() {
     return(
-        <nav>
-            header
+        <nav className="">
+            <div className="">
+                <img alt="" src="images/header/logo.png"/>
+            </div>
+            <div className="">
+                <button>About</button>
+                <button>How it works</button>
+                <button>Pricing</button>
+            </div>
+
+            <div className="">
+                <button>Download App</button>
+            </div>
         </nav>
     )
 }
