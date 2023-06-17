8292df7db538ffbd3190d61541bc76ce6ae8f8bf
Yubin <yubinjodev@gmail.com>
2023-06-17 15:06:01 +0900

    download feature

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,16 @@ import "../../../styles/global.scss";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Download from "@/features/home/Download";
 
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Download/>
+      </main>
       <Footer />
     </>
   );
