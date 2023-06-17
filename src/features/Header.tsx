087f0c74de2f1da6fe4b982c5377f689f4d08ee3
Yubin <yubinjodev@gmail.com>
2023-06-17 14:25:32 +0900

    dialog logic + semantic nav

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -1,20 +1,64 @@
-import '../../styles/pages/_header.scss'
+"use client";
+
+import "../../styles/pages/_header.scss";
+
+import { useState } from "react";
+
+import QrDownloadDialog, {
+  QrDownloadDialogProps,
+} from "@/dialogs/QrDownloadDialog";
+
+type DialogId = "QrDownloadDialog";
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <img alt="" src="images/header/logo.png" />
-      </div>
-      <nav className="header__controls">
-        <button>About</button>
-        <button>How it works</button>
-        <button>Pricing</button>
-      </nav>
+  const [dialogId, setDialogId] = useState<DialogId>();
+  const [qrDownloadDialogProps, setQrDownloagDialogProps] =
+    useState<QrDownloadDialogProps>();
 
-      <div className="header__download">
-        <button>Download App</button>
-      </div>
-    </header>
+  const closeDialog = () => {
+    setDialogId(undefined);
+    setQrDownloagDialogProps(undefined);
+  };
+
+  const openDownloadDialog = () => {
+    setDialogId("QrDownloadDialog");
+    setQrDownloagDialogProps({
+      open: true,
+      close: closeDialog,
+    });
+  };
+
+  const handleClickDownload = () => {
+    openDownloadDialog();
+  };
+
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <img alt="" src="images/header/logo.png" />
+        </div>
+        <nav className="header__controls">
+          <ul>
+            <li>About</li>
+            <li>How it works</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+          </ul>
+          {/* <button>About</button>
+          <button>How it works</button>
+          <button>Pricing</button>
+          <button>Blog</button> */}
+        </nav>
+
+        <div className="header__download">
+          <button onClick={handleClickDownload}>Download App</button>
+        </div>
+      </header>
+
+      {dialogId === "QrDownloadDialog" && qrDownloadDialogProps && (
+        <QrDownloadDialog {...qrDownloadDialogProps} />
+      )}
+    </>
   );
 }
