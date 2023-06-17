2ef0f2ee9cecf17787fad00ebdf67017721aeb21
Yubin <yubinjodev@gmail.com>
2023-06-17 16:24:44 +0900

    qr dialog done. YAS

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,29 @@
 import "../../styles/pages/_footer.scss";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import {
-    faPhone, faEnvelope, fa0
-  } from "@fortawesome/free-solid-svg-icons";
-
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__main">
         <div className="footer__main__logo">
-            <img alt="" src="/images/header/logo.png"/>
+          <img alt="" src="/images/header/logo.png" />
         </div>
         <div className="footer__main__title">
           You hate returns. We love them! We pick up right from your doorstep.
         </div>
         <div className="footer__main__social">
-                <FontAwesomeIcon id="icon" icon={faFacebookF} />
-                <FontAwesomeIcon id="icon" icon={faTwitter} />
-                <FontAwesomeIcon id="icon" icon={faInstagram} />
-                <FontAwesomeIcon id="icon" icon={faLinkedin} />
+          <FontAwesomeIcon id="icon" icon={faFacebookF} />
+          <FontAwesomeIcon id="icon" icon={faTwitter} />
+          <FontAwesomeIcon id="icon" icon={faInstagram} />
+          <FontAwesomeIcon id="icon" icon={faLinkedin} />
         </div>
       </div>
       <div className="footer__map">
@@ -39,11 +41,20 @@ export default function Footer() {
         <div className="footer__support__title">Support</div>
         <ul className="footer__support__list">
           <li>FAQ</li>
-          <li><span><FontAwesomeIcon id="icon" icon={faPhone} /></span>(888) 787 - 3027</li>
-          <li><span><FontAwesomeIcon id="icon" icon={faEnvelope} /></span>support@returnqueen.com</li>
+          <li>
+            <span>
+              <FontAwesomeIcon id="icon" icon={faPhone} />
+            </span>
+            (888) 787 - 3027
+          </li>
+          <li>
+            <span>
+              <FontAwesomeIcon id="icon" icon={faEnvelope} />
+            </span>
+            support@returnqueen.com
+          </li>
         </ul>
       </div>
-      
     </footer>
   );
 }
--- src/dialogs/QrDownloadDialog.tsx
+++ src/dialogs/QrDownloadDialog.tsx
@@ -1,21 +1,39 @@
 import "../../styles/components/_dialog.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+
 export type QrDownloadDialogProps = {
   open: boolean;
   close: () => void;
 };
 
-export default function QrDownloadDialog(props:QrDownloadDialogProps) {
-    const {open,  close} = props;
+export default function QrDownloadDialog(props: QrDownloadDialogProps) {
+  const { open, close } = props;
 
-    const handleClickCloseDialog=()=>{
-        close();
-    }
+  const handleClickCloseDialog = () => {
+    close();
+  };
 
   return (
-    <dialog className="qrdownloaddialog">
-      <>hello</>
-      <button onClick={handleClickCloseDialog}>close</button>
+    <dialog open={open} className="qrdownloaddialog" id="qrdownloaddialog">
+      <div className="qrdownloaddialog__top">
+        <p className="qrdownloaddialog__top__title">Scan this QR code</p>
+        <button
+          onClick={handleClickCloseDialog}
+          className="qrdownloaddialog__top__action"
+        >
+          <FontAwesomeIcon id="icon" icon={faClose} />
+        </button>
+      </div>
+
+      <div className="qrdownloaddialog__content">
+        <div className="qrdownloaddialog__content__qrcode">
+          <img alt="" src="/images/dialog/qrcode/qrcode.png"/>
+        </div>
+
+        <p className="qrdownloaddialog__content__title">Download the ReturnQueen App</p>
+      </div>
     </dialog>
   );
 }
