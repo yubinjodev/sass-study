e787d18b9b2451c3c23651c8846bcc59ed6a65dc
Yubin <yubinjodev@gmail.com>
2023-06-20 16:33:25 +0900

    about done!

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -26,12 +26,12 @@ export default function About() {
     </div>
 
     <div className="about__bottom">
-        <div className="about__bottom_left">
+        <div className="about__bottom__left">
             <p>Between hectic schedules and conflicting priorities, moms Daphna and Dasya struggled to meet their return deadlines. One thing had become clear: Returns needed an upgrade. They thought: Wouldn’t it be nice if you could tap a button to make all the boxes disappear—and watch the refunds roll in?</p>
             <p>Many, many, MANY iced espressos with almond milk later, ReturnQueen was born with a specific mission:</p>
             <p className="about__bottom__title">provide fast, easy, stress-free returns for all.</p>
         </div>
-        <div className="about__bottom_right">
+        <div className="about__bottom__right">
             <p>Because let’s be honest.</p>
             <p>We all could afford to save some time and money. Whether you’re a parent in desperate need of more free time, busy with back-to-back meetings, or somebody who just prefers to order two sizes and three colors of the same style, you deserve to spend your time doing the things you love.</p>
             <p>So leave the returns to us!</p>
